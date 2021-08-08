1c51f39fe6e99da4c75e3fbd367739b0ab4231b6
simmiggrwl <83505733+simmiggrwl@users.noreply.github.com>
2021-08-08 15:10:21 +0530

    Set up react

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt= require ('jsonwebtoken');
 module.exports = (req,res,next) => {
     if(req.headers['authorization']){
-        const token= req.heders.authorization.split(' ')[1];
+        const token= req.headers.authorization.split(' ')[1];
         jwt.verify(token, process.env.SECRET, (err, decoded) => {
             if(err){
                 next(Error('Failed to authenticate token'));
